32a5c654adf993965013321128498a747c3f3c11
ChandanKhamitkar <khamitkar.dev@gmail.com>
2025-08-16 20:51:23 +0530

    Add missing links footer
    Signed-off-by: ChandanKhamitkar <khamitkar.dev@gmail.com>

--- src/lib/footer.ts
+++ src/lib/footer.ts
@@ -3,15 +3,15 @@ export const linkSections = [
     title: "Links",
     items: [
       { label: "Github", href: "https://github.com/ChandanKhamitkar/BranchDB" },
-      { label: "Developer", href: "https://github.com/ChandanKhamitkar" },
+      { label: "Developer", href: "/#meet_developer" },
     ],
   },
   {
     title: "Docs",
     items: [
       { label: "Getting Started", href: "/docs" },
-      { label: "Core Features", href: "/docs/" },
-      { label: "Client SDK's", href: "" },
+      { label: "Core Features", href: "/docs/concepts" },
+      { label: "Client SDK's", href: "/docs/sdk" },
     ],
   },
   {
