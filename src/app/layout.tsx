1947f3f61c7055a1f64c7cbcd676da302658e31c
ChandanKhamitkar <khamitkar.dev@gmail.com>
2025-08-20 16:11:51 +0530

    add keywords to layout.tsx
    Signed-off-by: ChandanKhamitkar <khamitkar.dev@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,34 @@ import "./globals.css";
 import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AOSProvider from './AOSProvider';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "BranchDB",
   description: "BranchDB a lightweight a Key-Value Store, with in-memory data & disk persistence built in C++.",
+  keywords: [
+    "BranchDB",
+    "Branch DB",
+    "key-value store",
+    "key value store",
+    "in-memory database",
+    "in memory database",
+    "C++ database",
+    "Cpp database",
+    "from scratch database",
+    "persistence",
+    "WAL",
+    "Write-Ahead Log",
+    "multithreaded database",
+    "concurrent",
+    "TTL",
+    "log compaction",
+    "binary protocol",
+    "self-hosted",
+    "Docker",
+    "open-source database",
+    "developer tools"
+  ],
 };
 
 export default function RootLayout({
